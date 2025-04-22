467ff7ded20792a344a1da045a2efa4996631778
MuumiTikkari <niko23010@student.hamk.fi>
2025-04-22 14:03:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// View engine
-app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'main' }));
-app.set('view engine', 'hbs');
+// View engine setup
+app.engine('handlebars', exphbs.engine({
+    extname: '.handlebars',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+  }));
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 // Routes
