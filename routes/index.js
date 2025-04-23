0df37d591ea0b192ff5954c2b0c7ed8cd3afdc85
MuumiTikkari <niko23010@student.hamk.fi>
2025-04-23 13:08:59 +0300

    home page

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./auth');
+const homeRoutes = require('./home')
 
+router.use('/', homeRoutes);
 router.use('/auth', authRoutes);
 
-router.get('/', (req, res) => {
-  if(req.user){ // If user is logged in, send user info to homepage
-    const user = req.user.toObject();
-    console.log(user); // REMOVE THIS
-    res.render('home', { title: 'Home Page', user });
-  }else{
-    res.render('home', { title: 'Home Page' });
-  }
-});
+
 
 
 
