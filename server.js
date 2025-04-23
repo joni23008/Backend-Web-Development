ce8fa6d680f40d00a4a1c5dbc7ef1b4084b0c7e1
Joni Mitronen <142889693+joni23008@users.noreply.github.com>
2025-04-23 14:01:00 +0300

    commit

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const path = require("path");
-const reviewRoutes = require("./routes/reviewRoutes");
+const session = require("express-session");
+const passport = require("passport");
+const flash = require("connect-flash");
 require("dotenv").config();
 
 const app = express();
@@ -10,10 +12,39 @@ const app = express();
 // DB connection
 require("./config/db")();
 
-// Middleware
+// Passport config
+require("./config/passport")(passport);
+
+// Body parsers
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Static files
 app.use(express.static(path.join(__dirname, "public")));
+
+// Session middleware (must come before passport)
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "fallback_string",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware to attach the logged-in user to views
+app.use((req, res, next) => {
+  res.locals.user = req.user || null; // Attach the logged-in user (or null if not logged in)
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error"); // for passport errors
+  next();
+});
 
 // View engine setup
 app.engine(
@@ -28,11 +59,7 @@ app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 
 // Routes
-// reviews routes
-app.use("/api/review", reviewRoutes);
-// root route
-app.get("/", (req, res) => {
-  res.status(200).send("Let's start to review some movies!");
-});
+const mainRoutes = require("./routes/index");
+app.use("/", mainRoutes);
 
 module.exports = app;
