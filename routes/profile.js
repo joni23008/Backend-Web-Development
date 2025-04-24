192cf2ab63ec916df356dc3cc8bdb3b4a2fff84f
MuumiTikkari <niko23010@student.hamk.fi>
2025-04-24 15:31:08 +0300

    Profile deleting added

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { isAuthenticated } = require('../middlewares/auth');
 const Review = require('../models/Review');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 // GET /profile
 router.get('/', isAuthenticated, async (req, res) => {
@@ -20,5 +21,33 @@ router.get('/', isAuthenticated, async (req, res) => {
       res.redirect('/');
     }
   });
+
+// POST /profile/delete
+router.post('/delete', isAuthenticated, async (req, res) => {
+    const { password } = req.body;
+
+    try {
+        const user = await User.findById(req.user._id);
+        const isMatch = await bcrypt.compare(password, user.password);
+    
+        if (!isMatch) {
+          req.flash('error', 'Incorrect password');
+          return res.redirect('/profile');
+        }
+    
+        await Review.deleteMany({ user: req.user._id });
+        await User.findByIdAndDelete(req.user._id);
+    
+        req.logout(function(err) {
+          if (err) return next(err);
+          req.flash('success', 'Account deleted successfully');
+          res.redirect('/');
+        });
+      } catch (err) {
+        console.error(err);
+        req.flash('error', 'Failed to delete account');
+        res.redirect('/profile');
+      }
+    });
   
   module.exports = router;
