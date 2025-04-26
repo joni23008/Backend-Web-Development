4df149d1557e01b53ebd368d64053bc872b7f9ec
Teemu eemeli Tupola <Teemu.tupola@student.hamk.fi>
2025-04-26 21:48:12 +0300

    delete DBTESTI tiedot

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -3,13 +3,11 @@ const router         = express.Router();
 
 const homeRoutes     = require("./home");
 const authRoutes     = require("./auth");
-const dbRouter       = require("./DB");
 const reviewRoutes   = require("./reviewRoutes");
 const apiRoutes      = require("./api");      
 
 router.use("/",           homeRoutes);
 router.use("/auth",       authRoutes);
-router.use("/DB",         dbRouter);
 router.use("/api/review", reviewRoutes);
 router.use("/",        apiRoutes);       
 
