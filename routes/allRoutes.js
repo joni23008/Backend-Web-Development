354ac3f9732e0adfc0415e506b44349e25f13411
MuumiTikkari <niko23010@student.hamk.fi>
2025-09-26 19:02:54 +0300

    Added routes for mobile auth + modified auth check

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -6,11 +6,13 @@ const authRoutes = require("./auth");
 const reviewRoutes = require("./reviewRoutes");
 const profileRoutes = require("./profile");
 const apiRoutes = require("./api");
+const mobileAuthRoutes = require("./mobileAuth");
 
 router.use("/", homeRoutes);
 router.use("/auth", authRoutes);
 router.use("/review", reviewRoutes);
 router.use("/profile", profileRoutes);
+router.use("/mobileAuth", mobileAuthRoutes);
 router.use("/", apiRoutes);
 
 module.exports = router;
